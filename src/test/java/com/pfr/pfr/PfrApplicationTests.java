1573cebbe7a72b80d291c9b0eeaa22587d469371
Killian Godet <godetkillian@yahoo.com>
2023-05-09 12:11:11 +0200

    init

---
+++ src/test/java/com/pfr/pfr/PfrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfr.pfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
