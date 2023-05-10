3869f6ae88022f29ae78a9019463e4af0939f228
Killian Godet <godetkillian@yahoo.com>
2023-05-10 15:00:54 +0200

    add cors policies in application.properties + LocationController

--- src/main/java/com/pfr/pfr/location/LocationController.java
+++ src/main/java/com/pfr/pfr/location/LocationController.java
@@ -3,6 +3,7 @@ package com.pfr.pfr.location;
 import com.pfr.pfr.entities.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/location")
 @Validated
+@CrossOrigin(origins = {"${app.api.settings.cross-origin.url}"})
 public class LocationController {
 
     @Autowired
