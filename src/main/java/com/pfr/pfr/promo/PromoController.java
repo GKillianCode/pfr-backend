9acfaccba376f8a085151c874972bebe35ee55f1
louise-midrange <louise.roy@hotmail.fr>
2023-05-10 12:02:32 +0200

    event entity created with repo/service/controller & test
    Add PromoWithEvents class with API point
    Add test for PromoWIthEvents API point

--- src/main/java/com/pfr/pfr/promo/PromoController.java
+++ src/main/java/com/pfr/pfr/promo/PromoController.java
@@ -1,10 +1,11 @@
 package com.pfr.pfr.promo;
 
-import com.pfr.pfr.entities.Location;
 import com.pfr.pfr.entities.Promo;
+import com.pfr.pfr.promo.dto.PromoWithEvents;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +22,8 @@ public class PromoController {
     @GetMapping("/all")
     public List<Promo> getAllPromos() { return promoService.getAll(); }
 
+    @GetMapping("/{id}/events")
+    public PromoWithEvents getPromoWithEvents(@PathVariable("id") Integer promoId) {
+        return promoService.getPromoWithEvents(promoId);
+    }
 }
