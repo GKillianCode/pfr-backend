92f9376832ff56154644048daa45c74377abd570
Chesneau Luc <luc.chesneau41@gmail.com>
2023-05-11 10:47:24 +0200

    Resolve conflict UserController

--- src/main/java/com/pfr/pfr/user/UserController.java
+++ src/main/java/com/pfr/pfr/user/UserController.java
@@ -3,7 +3,11 @@ package com.pfr.pfr.user;
 import com.pfr.pfr.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,6 +22,16 @@ public class UserController {
 
     @GetMapping("/all")
     public List<User> getAllUsers() { return userService.getAll(); }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable("id") Integer userId) {
+        return userService.getById(userId);
+    }
+
+    @GetMapping("/{id}/promos")
+    public UserWithPromos getUserWithPromos(@PathVariable("id") Integer userId) {
+        return userService.getUserWithPromos(userId);
+    }
 
     @PostMapping("/connect")
     public String connect(@RequestBody User user) {
