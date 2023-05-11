381ba388514b10520abb3676ea6d928b1f5b9600
Patrick Yim <>
2023-05-11 15:00:20 +0200

    swagger added to booking / classroom / event / event type / location / promo / role / slot

--- src/main/java/com/pfr/pfr/role/RoleController.java
+++ src/main/java/com/pfr/pfr/role/RoleController.java
@@ -1,6 +1,12 @@
 package com.pfr.pfr.role;
 
 import com.pfr.pfr.entities.Role;
+import com.pfr.pfr.exceptions.ExceptionMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,6 +25,20 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
+    @Operation(summary = "Get all roles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Role.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid supplied", content = @Content(mediaType = "application/json", schema = @Schema(oneOf = {
+                    ExceptionMessage.class }))),
+            @ApiResponse(responseCode = "403", description = "Access denied", content = @Content(mediaType = "application/json", schema = @Schema(oneOf = {
+                    ExceptionMessage.class }))),
+            @ApiResponse(responseCode = "404", description = "Roles not found", content = @Content(mediaType = "application/json", schema = @Schema(oneOf = {
+                    ExceptionMessage.class }))),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(oneOf = {
+                    ExceptionMessage.class })))
+    })
     @GetMapping("/all")
     public List<Role> getAllRoles() { return roleService.getAll(); }
 }
