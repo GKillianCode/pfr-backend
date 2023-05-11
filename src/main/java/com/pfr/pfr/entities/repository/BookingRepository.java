3834b83d276960e16c54d1dc65a4de002b99ce2a
Chesneau Luc <luc.chesneau41@gmail.com>
2023-05-11 17:05:45 +0200

    Adding DTO UserWithBookings

--- src/main/java/com/pfr/pfr/entities/repository/BookingRepository.java
+++ src/main/java/com/pfr/pfr/entities/repository/BookingRepository.java
@@ -4,5 +4,11 @@ import com.pfr.pfr.entities.Booking;
 import com.pfr.pfr.entities.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+    public List<Booking> findAllByUser_Id(Integer id);
+
+
 }
---
+++ src/main/java/com/pfr/pfr/user/dto/UserWithBookings.java
@@ -0,0 +1,20 @@
+package com.pfr.pfr.user.dto;
+
+import com.pfr.pfr.entities.Booking;
+import com.pfr.pfr.entities.User;
+import com.pfr.pfr.user.UserService;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = {"user", "bookings"})
+public class UserWithBookings {
+    private User user;
+    private List<Booking> bookings;
+
+}
