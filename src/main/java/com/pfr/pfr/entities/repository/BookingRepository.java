184c86314ffc59386700dfd4299324493f1f4eee
louise-midrange <louise.roy@hotmail.fr>
2023-05-12 11:54:42 +0200

    add user bookings with pagination

--- src/main/java/com/pfr/pfr/entities/repository/BookingRepository.java
+++ src/main/java/com/pfr/pfr/entities/repository/BookingRepository.java
@@ -1,16 +1,13 @@
 package com.pfr.pfr.entities.repository;
 
 import com.pfr.pfr.entities.Booking;
-import com.pfr.pfr.entities.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-import java.util.List;
-
-import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
@@ -19,10 +16,11 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
     @Query("SELECT b FROM Booking b WHERE b.event.promo.id = :promoId")
     List<Booking> findByPromoId(@Param("promoId") Integer promoId);
 
-
-    public List<Booking> findAllByUser_Id(Integer id);
+    public Integer countByUserId(Integer userId);
+    public List<Booking> findAllByUserIdOrderByBookingDateAscSlotAsc(Integer id, Pageable pageable);
 
     public List<Booking> findAllByEventId(Integer id);
+
 
 
 }
