73ec8056fa8140f62e6a963de9d696e898f8b1a6
Chesneau Luc <luc.chesneau41@gmail.com>
2023-05-17 11:56:33 +0200

    CRUD for Conflict, (without updating). Add ConflictDTO and tests

--- src/main/java/com/pfr/pfr/conflict/ConflictService.java
+++ src/main/java/com/pfr/pfr/conflict/ConflictService.java
@@ -2,10 +2,13 @@ package com.pfr.pfr.conflict;
 
 import com.pfr.pfr.entities.Conflict;
 import com.pfr.pfr.entities.repository.ConflictRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.management.InstanceAlreadyExistsException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ConflictService {
@@ -17,5 +20,31 @@ public class ConflictService {
 
     public List<Conflict> getConflictsForBooking(Integer bookingId) {
         return conflictRepository.findByBookingId(bookingId);
+    }
+
+    public Conflict getById(Integer id) throws EntityNotFoundException {
+        Optional<Conflict> conflict = conflictRepository.findById(id);
+        if(conflict.isPresent()) {
+            return conflict.get();
+        } else {
+            throw new EntityNotFoundException("No conflict found with id " + id);
+        }
+    }
+
+    public Conflict save(Conflict conflict) throws InstanceAlreadyExistsException {
+        List<Conflict> conflicts = conflictRepository.findByBookingId(conflict.getId());
+            if(conflicts.contains(conflict)){
+                throw new InstanceAlreadyExistsException("Conflict already exists");
+            }
+        return conflictRepository.save(conflict);
+    }
+
+    public void delete(Integer id) throws EntityNotFoundException {
+        Optional<Conflict> conflict = conflictRepository.findById(id);
+        if(conflict.isPresent()) {
+            conflictRepository.delete(conflict.get());
+        } else {
+            throw new EntityNotFoundException("No conflict found with id " + id);
+        }
     }
 }
--- src/main/java/com/pfr/pfr/entities/Conflict.java
+++ src/main/java/com/pfr/pfr/entities/Conflict.java
@@ -28,17 +28,38 @@ public class Conflict {
     @Column(name="created_at")
     private LocalDateTime createdAt;
 
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
     @ManyToOne
-    @JoinColumn(name ="booking_id")
+    @JoinColumn(name ="booking_id", insertable=false, updatable=false)
     private Booking booking;
 
-    @ManyToOne
-    @JoinColumn(name ="user_id")
-    private User user;
+    @Column(name="booking_id")
+    private Integer bookingId;
 
     @ManyToOne
-    @JoinColumn(name ="event_id")
+    @JoinColumn(name ="user_id", insertable=false, updatable=false)
+    private User user;
+
+    @Column(name="user_id")
+    private Integer userId;
+
+    @ManyToOne
+    @JoinColumn(name ="event_id", insertable=false, updatable=false)
     private Event event;
+
+    @Column(name="event_id")
+    private Integer eventId;
+
+    public Conflict(String comment, Booking booking, User user, Event event) {
+        this.comment = comment;
+        this.booking = booking;
+        this.user = user;
+        this.event = event;
+    }
 
     public Conflict(String comment, LocalDateTime createdAt, Booking booking, User user, Event event) {
         this.comment = comment;
@@ -46,5 +67,20 @@ public class Conflict {
         this.booking = booking;
         this.user = user;
         this.event = event;
+    }
+
+    public Conflict(String comment, Integer bookingId, Integer userId, Integer eventId) {
+        this.comment = comment;
+        this.bookingId = bookingId;
+        this.userId = userId;
+        this.eventId = eventId;
+    }
+
+    public Conflict(String comment, LocalDateTime createdAt, Integer bookingId, Integer userId, Integer eventId) {
+        this.comment = comment;
+        this.bookingId = bookingId;
+        this.createdAt = createdAt;
+        this.userId = userId;
+        this.eventId = eventId;
     }
 }
